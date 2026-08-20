4f59f45050188b2ee368db5985e5a0a01d286a78
mane-gonzalez <60636080+mane-gonzalez@users.noreply.github.com>
2026-08-20 20:09:09 +0200

    Add: time-based-key-value-store - submission-5

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-5.cpp
@@ -0,0 +1,35 @@
+class TimeMap {
+private:
+    unordered_map<string, vector<pair<string, int>>> map;
+public:
+    TimeMap() {
+        
+    }
+    
+    void set(string key, string value, int timestamp) {
+        map[key].push_back({value, timestamp});
+    }
+    
+    string get(string key, int timestamp) {
+        const vector<pair<string, int>>& placeholder = map[key];
+
+        string result = "";
+
+        int left = 0;
+        int right = (int)placeholder.size() - 1;
+
+        while(left <= right) {
+            int mid = left + (right - left)/2;
+
+            if(placeholder[mid].second <= timestamp) {
+                result = placeholder[mid].first;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return result;
+        
+    }
+};
