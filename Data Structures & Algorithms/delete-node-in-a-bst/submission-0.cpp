ff3032508ceecece8aee0dc5f1b1a0a56f01b250
mane-gonzalez <60636080+mane-gonzalez@users.noreply.github.com>
2026-09-23 21:13:18 +0200

    Add: delete-node-in-a-bst - submission-0

---
+++ Data Structures & Algorithms/delete-node-in-a-bst/submission-0.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if (!root) return root;
+
+        if (key > root->val) {
+            root->right = deleteNode(root->right, key);
+        } else if (key < root->val) {
+            root->left = deleteNode(root->left, key);
+        } else {
+            if (!root->left) return root->right;
+            if (!root->right) return root->left;
+
+            TreeNode* cur = root->right;
+            while (cur->left) {
+                cur = cur->left;
+            }
+            root->val = cur->val;
+            root->right = deleteNode(root->right, root->val);
+        }
+
+        return root;
+    }
+};
